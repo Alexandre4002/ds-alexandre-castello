0315c02b757e049b76c2ad8fcae2b8275020b706
Alexandre gatao <juniorbrasolin3@gmail.com>
2025-02-24 09:31:33 -0300

    ok

---
+++ aula02/variaveis.java
@@ -0,0 +1,18 @@
+public class variaveis {
+  public static void main(String[] args) {
+    String msg = "Alexandre gatao";
+    System.out.println(msg);
+    int n1 = 3;
+    int n2 = 4;
+    float n3 = 6.99f;
+    char C= '5';
+    boolean dedomindinho = false;
+    String Kikobahiano = "João sifilis é um bom professor";
+    System.out.println(n1);
+    System.out.println(n1 + n2);
+    System.out.println(n3+n1+n2);
+    System.out.println(C);
+    System.out.println(dedomindinho); 
+    System.out.println(Kikobahiano);    
+  }
+}
