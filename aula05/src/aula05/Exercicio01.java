418941f417e75edd3197d7a300882fe0ef378f0b
Alexandre Castello <juniorbrasolin3@gmail.com>
2025-04-28 09:19:51 -0300

    aula 05 sobre java e sobre o ide netbeans

---
+++ aula05/src/aula05/Exercicio01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+import java.util.Scanner;
+/**
+ *
+ * @author CAMARGO
+ */
+public class Exercicio01 {
+     public static void main(String[] args) {
+         int  gastosJaneiro = 15000;
+         int  gastosFevereiro = 17000;
+         int gastosMarco = 25000;
+         int gastosTrimestre = gastosJaneiro+gastosFevereiro+gastosMarco;
+         
+         System.out.println("O valor gasto no trimestre foi de:" +gastosTrimestre);
+    }
+} 
